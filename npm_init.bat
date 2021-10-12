5d806bbb5e79373a4a58e740c855f00d27647a9a
Hulkt1m0Hom8r3 <92400209+Hulkt1m0Hom8r3@users.noreply.github.com>
2021-10-12 15:12:22 -0500

    Update npm_init.bat

--- npm_init.bat
+++ npm_init.bat
@@ -1,4 +1,9 @@
-$ npx license mit > LICENSE
-$ npx gitignore node
-$ npx covgen fanella.patrick@gmail.com
-$ npm init -y
+function node-project {
+  git init
+  npx license $(npm get init.license) -o "$(npm get init.author.name)" > LICENSE
+  npx gitignore node
+  npx covgen "$(npm get init.author.email)"
+  npm init -y
+  git add -A
+  git commit -m "Initial commit"
+}
